d7116750bba0603fa8f7581bc8e1d94ebbe18897
Mike Jones <jones@cs.byu.edu>
2018-06-25 17:23:54 -0600

    simple threaded sockets and client

--- client/client.py
+++ client/client.py
@@ -1,42 +1,9 @@
-import sys
-from socket import socket
-
-MSGLEN = 128
-
-class MySocket:
-    #see https://docs.python.org/3/howto/sockets.html"
-    #""demonstration class only
-    #  - coded for clarity, not efficiency
-    #"""
-
-    def __init__(self, sock=None):
-        if sock is None:
-            self.sock = socket.socket(
-                            socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = sock
-
-    def connect(self, host, port):
-        self.sock.connect((host, port))
-
-    def mysend(self, msg):
-        totalsent = 0
-        while totalsent < MSGLEN:
-            sent = self.sock.send(msg[totalsent:])
-            if sent == 0:
-                raise RuntimeError("socket connection broken")
-            totalsent = totalsent + sent
-
-    def myreceive(self):
-        chunks = []
-        bytes_recd = 0
-        while bytes_recd < MSGLEN:
-            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
-            if chunk == b'':
-                raise RuntimeError("socket connection broken")
-            chunks.append(chunk)
-            bytes_recd = bytes_recd + len(chunk)
-        return b''.join(chunks)
+import socket
+import threading
 
 if __name__ == "__main__":
-    print ("hello cruel world!")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.connect(('10.24.66.30', 1234))
+    sock.send('hello over there')
+

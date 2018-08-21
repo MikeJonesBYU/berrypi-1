8d708d3d96ffaecd0e4010db35fd21aa8c38c875
bencrowder <ben.crowder@gmail.com>
2018-08-21 08:05:41 -0600

    Adds TODO

--- berry/berries/button.py
+++ berry/berries/button.py
@@ -4,6 +4,7 @@ module (so that we can reload the module dynamically and update code).
 """
 from ..berrybase import BerryBase
 
+# TODO: change this to use client/handlers
 import berry.berries.button_handlers
 
 
@@ -12,10 +13,12 @@ class BerryButton(BerryBase):
         """
         Stub for the on_press handler. Activated when the button is pressed.
         """
+        # TODO: change this to use client/handlers
         button_handlers.on_press()
 
     def on_release():
         """
         Stub for the on_release handler. Activated when the button is released.
         """
+        # TODO: change this to use client/handlers
         button_handlers.on_release()

17f3eea3444549f39b26c70d4e1317904a2df97d
bencrowder <ben.crowder@gmail.com>
2018-08-25 15:51:30 -0600

    Initial attempt to integrate gpiozero

--- berry/berries/button.py
+++ berry/berries/button.py
@@ -2,10 +2,22 @@
 Button class. Each stub method calls the appropriate function in the handlers
 module (so that we can reload the module dynamically and update code).
 """
+from gpiozero import Button
+
 from ..berrybase import BerryBase
 
 
 class BerryButton(BerryBase):
+    _button = None
+
+    def __init__(self):
+        # Hook up to gpiozero, using pin GP17
+        self._button = Button(17)
+
+        # Hook in handlers
+        self._button.when_pressed = self.on_press
+        self._button.when_released = self.on_release
+
     def on_press(self):
         """
         Stub for the on_press handler. Activated when the button is pressed.

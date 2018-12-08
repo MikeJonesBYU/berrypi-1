c2ab9f99f7dd888acd99b411806d80497f163baa
bencrowder <ben.crowder@gmail.com>
2018-12-07 21:03:05 -0700

    Adds FSR force loop

--- berry/berries/fsr.py
+++ berry/berries/fsr.py
@@ -4,14 +4,20 @@ module (so that we can reload the module dynamically and update code).
 """
 from ..berrybase import BerryBase
 
+import threading
+import time
+
 
 class BerryFSR(BerryBase):
     _sensor = None
-    _divisor = 0.0004885197850512668
-    _test_force = 50  # Used for internal testing
+    _divisor = 0.005
+    _sampling_rate = 0.1  # 100ms
+    _test_force = 0.5  # Used for internal testing
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        self._average_force = 0.001
 
     def initialize_gpio(self):
         """
@@ -27,15 +33,40 @@ class BerryFSR(BerryBase):
             # bother initializing GPIO
             return
 
-    def _scale(self, value):
-        return self._divisor / value
+        # Start force loop thread
+        threading.Thread(target=self.force_loop).start()
+
+    def _force_loop(self):
+        """
+        Thread that polls to get the current force on the FSR. Populates the
+        self._average_force value.
+        """
+        NUM_SAMPLES = 10.0
+
+        # Get the initial readings
+        time.sleep(1)
+        readings = []
+        for i in range(0, int(NUM_SAMPLES)):
+            readings.insert(0, self._sensor.value)
+            time.sleep(self._sampling_rate)
+
+        self._average_force = sum(r for r in readings) / NUM_SAMPLES
+
+        # Average the readings
+        while True:
+            readings.insert(0, self._sensor.value)
+            readings.pop()
+
+            self._average_force = sum(r for r in readings) / NUM_SAMPLES
+
+            time.sleep(self._sampling_rate)
 
     def force(self):
         """
         Part of force API. Returns a number with the scaled force (which should
-        end up being between 0 and 1 based on my testing).
+        end up being between 0 and about 5 based on my testing).
         """
-        return self._scale(self.force())
+        return self._divisor / self._average_force
 
     def raw_force(self):
         """

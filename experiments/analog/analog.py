8e8e7802e16a4b33c2c6440d8cb17099cdef40d1
Ben Crowder <ben.crowder@gmail.com>
2018-12-07 20:57:23 -0700

    Adds analog stuff

--- experiments/analog/analog.py
+++ experiments/analog/analog.py
@@ -4,8 +4,24 @@ import time
 
 from gpiozero import MCP3008
 
+divisor = 0.0004885197850512668
+divisor = 0.005
 pot = MCP3008(channel=0)
 
+time.sleep(1)
+readings = []
+for i in range(0, 10):
+    readings.insert(0, pot.value)
+    time.sleep(0.05)
+
+average_force = sum(r for r in readings) / 10.0
+
 while True:
-    print(0.0004885197850512668 / pot.value)
+    readings.insert(0, pot.value)
+    readings.pop()
+
+    average_force = sum(r for r in readings) / 10.0
+    scaled_force = divisor / average_force
+
+    print(round(scaled_force, 1))
     time.sleep(0.1)

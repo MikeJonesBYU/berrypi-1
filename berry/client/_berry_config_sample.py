aab6cd2f83a4e3868bade0052bc13a5d35d9a9c9
bencrowder <ben.crowder@gmail.com>
2019-02-04 21:41:40 -0700

    Adds widget setup script

--- berry/client/_berry_config_sample.py
+++ berry/client/_berry_config_sample.py
@@ -1,10 +1,10 @@
 # Berry-specific config
 #
-# Instructions: Copy this to _berry_config.py and then update line 12 to
+# Instructions: Copy this to _berry_config.py and then update line 10 to
 # instantiate the right class.
 
 from berry import berries
 
 
 def get_berry(guid):
-    return berries.BerryButton(live=False, guid=guid)
+    return berries.BerryButton(live=True, guid=guid)
---
+++ setupwidget.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+Script to easily set up (or reset) a widgetj.
+"""
+import shutil
+import sys
+
+if len(sys.argv) < 2:
+    print('Usage: python3 setupwidget.py [TYPE]')
+    print('Types: button | led | fsr | accelerometer | speaker | screen')
+    sys.exit(-1)
+
+# Get the slug
+slug = sys.argv[1]
+print('Setting up {} widget'.format(slug))
+
+# First, copy the handler sample over
+print('Clearing out handlers')
+
+src_path = 'berry/berries/{}_handlers.py'.format(slug)
+dest_path = 'berry/client/_handlers.py'
+
+try:
+    shutil.copyfile(src_path, dest_path)
+except OSError as ex:
+    print('Error copying handlers: {}'.format(ex))
+
+# Then update the config (changing the class name appropriately)
+print('Updating config')
+
+sample_config = 'berry/client/_berry_config_sample.py'
+output_config = 'berry/client/_berry_config.py'
+
+try:
+    with open(sample_config, 'r') as f:
+        text = f.read()
+except OSError as ex:
+    print('Error reading sample config: {}'.format(ex))
+
+replacements = {
+    'button': 'Button',
+    'led': 'LED',
+    'fsr': 'FSR',
+    'accelerometer': 'Accelerometer',
+    'speaker': 'Speaker',
+    'screen': 'Screen',
+}
+
+text = text.replace('BerryButton', 'Berry{}'.format(replacements[slug]))
+
+try:
+    with open(output_config, 'w') as f:
+        f.write(text)
+except OSError as ex:
+    print('Error writing widget config: {}'.format(ex))
+
+print('Done!')

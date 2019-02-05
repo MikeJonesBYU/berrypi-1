df7c6331c5c69fb1b3aafdbd781ee62b5338ee76
bencrowder <ben.crowder@gmail.com>
2019-02-05 10:21:35 -0700

    Updates setup script

--- setupwidget.py
+++ setupwidget.py
@@ -5,31 +5,52 @@ Script to easily set up (or reset) a widgetj.
 import shutil
 import sys
 
+src_path = 'berry/berries/SLUG_handlers.py'
+dest_path = 'berry/client/_handlers.py'
+
+sample_config = 'berry/client/_widget_config_sample.py'
+output_config = 'berry/client/_widget_config.py'
+
+widget_name_file = 'berry/client/_widget_name.txt'
+
+###############################################################################
+
 if len(sys.argv) < 2:
-    print('Usage: python3 setupwidget.py [TYPE]')
+    print('Usage: python3 setupwidget.py [TYPE] [NUM]')
     print('Types: button | led | fsr | accelerometer | speaker | screen')
     sys.exit(-1)
 
 # Get the slug
 slug = sys.argv[1]
+src_path = src_path.replace('SLUG', slug)
+
+# Get the number, if present
+if len(sys.argv) >= 3:
+    num = sys.argv[2]
+else:
+    num = None
+
+# Prep the widget name (with optional number)
+widget_name = slug
+if num is not None:
+    widget_name += str(num)
+
 print('Setting up {} widget'.format(slug))
+
+###############################################################################
 
 # First, copy the handler sample over
 print('Clearing out handlers')
-
-src_path = 'berry/berries/{}_handlers.py'.format(slug)
-dest_path = 'berry/client/_handlers.py'
 
 try:
     shutil.copyfile(src_path, dest_path)
 except OSError as ex:
     print('Error copying handlers: {}'.format(ex))
 
+###############################################################################
+
 # Then update the config (changing the class name appropriately)
 print('Updating config')
-
-sample_config = 'berry/client/_widget_config_sample.py'
-output_config = 'berry/client/_widget_config.py'
 
 try:
     with open(sample_config, 'r') as f:
@@ -53,5 +74,16 @@ try:
         f.write(text)
 except OSError as ex:
     print('Error writing widget config: {}'.format(ex))
+
+###############################################################################
+
+# Then update the widget name
+print('Updating widget name')
+
+try:
+    with open(widget_name_file, 'w') as f:
+        f.write(widget_name)
+except OSError as ex:
+    print('Error writing widget name: {}'.format(ex))
 
 print('Done!')

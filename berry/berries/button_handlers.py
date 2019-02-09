4c8aa6eb2cfb6548cdb51a74767fa072e650070b
bencrowder <ben.crowder@gmail.com>
2019-02-09 15:44:56 -0700

    Cleans up handlers

--- berry/berries/button_handlers.py
+++ berry/berries/button_handlers.py
@@ -1,65 +1,27 @@
-"""
-Button handlers
-
-To initialize a new button widget, copy this file to berry/client/_handlers.py
-and delete this line.
-
-"""
 import time
 
 
-def on_press(state, remote, widget):
-    """
-    Handler called when the button is pressed. The remote parameter gives
-    access to other berries' functionality.
-    """
-    # -------------------------------------
-    # Fill in your code here
-    print("Button pressed!")
-    # -------------------------------------
-
-
-def on_release(state, remote, widget):
-    """
-    Handler called when the button is released. The remote parameter gives
-    access to other berries' functionality.
-    """
-    # -------------------------------------
-    # Fill in your code here
-    print("Button released!")
-    # -------------------------------------
-
-
 def setup(state, remote, widget):
-    """
-    Run whenever the client code is loaded. Optional.
-    """
-    # -------------------------------------
-    # Fill in your code here
-    print('Setup')
-    # -------------------------------------
+    # Run whenever the client code is loaded. Optional.
+    pass
 
 
 def loop(state, remote, widget):
-    """
-    If present, looped over in its own thread. Use for widget logic where
-    needed.
-    """
-    # -------------------------------------
-    # Fill in your code here
+    # Looped over in its own thread. Use for widget logic if needed.
+    time.sleep(1)
+
+
+def on_statechange(state, remote, widget):
+    # Called whenever the state is updated
+    val = state.get('value')
+    print(val)
+
+
+def on_press(state, remote, widget):
+    # Called when the button is pressed
     pass
-    # print('Looping')
-    # time.sleep(1)
-    # -------------------------------------
 
 
-def on_state(state, remote, widget):
-    """
-    Handler called whenever state is updated.
-    """
-    # -------------------------------------
-    # Fill in your code here
-    print('State')
-    # val = state.get('value')
-    # print('on_state, val = {}'.format(val))
-    # -------------------------------------
+def on_release(state, remote, widget):
+    # Called when the button is released
+    pass
--- berry/berries/fsr_handlers.py
+++ berry/berries/fsr_handlers.py
@@ -1,43 +1,17 @@
-"""
-Pressure sensor handlers
-
-To initialize a new pressure sensor widget, copy this file to
-berry/client/_handlers.py and delete this line.
-
-"""
 import time
 
 
 def setup(state, remote, widget):
-    """
-    Run whenever the client code is loaded. Optional.
-    """
-    # -------------------------------------
-    # Fill in your code here
-    print('Setup')
-    # -------------------------------------
+    # Run whenever the client code is loaded. Optional.
+    pass
 
 
 def loop(state, remote, widget):
-    """
-    If present, looped over in its own thread. Use for widget logic where
-    needed.
-    """
-    # -------------------------------------
-    # Fill in your code here
-    pass
-    # print('Looping')
-    # time.sleep(1)
-    # -------------------------------------
+    # Looped over in its own thread. Use for widget logic if needed.
+    time.sleep(1)
 
 
-def on_state(state, remote, widget):
-    """
-    Handler called whenever state is updated.
-    """
-    # -------------------------------------
-    # Fill in your code here
-    print('State')
-    # val = state.get('value')
-    # print('on_state, val = {}'.format(val))
-    # -------------------------------------
+def on_statechange(state, remote, widget):
+    # Called whenever the state is updated
+    val = state.get('value')
+    print(val)
